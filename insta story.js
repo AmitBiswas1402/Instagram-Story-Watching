f0f34549052f113d3ea37a2be201b05e3de6b554
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-12-26 16:59:01 +0000

    insta story feature needed to be fixed

---
+++ insta story.js
@@ -0,0 +1,17 @@
+var arr = [{dp:"https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1564564244660-5d73c057f2d2?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}, {dp:"https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}, {dp:"https://images.unsplash.com/photo-1516446636564-c48e723868fc?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1531311243148-d2653fa84700?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}, {dp:"https://images.unsplash.com/photo-1532376783479-9b99134739a7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1595118810936-0669f47aadba?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}]
+
+var stories = document.querySelector(".stories")
+var clutter = ""
+
+arr.forEach(function(element, idx){
+    clutter+= `<div class="story">
+    <img id="${idx}" src="${element.dp}" alt="pic">
+    </div>`
+})
+
+stories.innerHTML = clutter
+
+stories.addEventListener("click", function(dets){
+    document.querySelector(".full-screen").style.display = "block"
+    document.querySelector(".full-screen").style.backgroundImage = `url(${arr[arr.dets.id].story})`
+})
